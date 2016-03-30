4acc23283baae3ea6b6b7675014c608b1e80ca48
Rifat Rashid <s-RRashid@lwsd.org>
2016-03-30 02:34:29 -0700

    project edits

--- app/src/main/java/com/example/seize/circledots/DotsGrid.java
+++ app/src/main/java/com/example/seize/circledots/DotsGrid.java
@@ -4,4 +4,11 @@ package com.example.seize.circledots;
  * Created by Seize on 3/30/2016.
  */
 public class DotsGrid {
+    public DotsGrid(){
+
+    }
+
+    public DotsGrid(int screenWidth, int screenHeight, int dotRadius){
+
+    }
 }
